76928e167b600761c97326cb95cd5061d30f890b
Student <student@client2516.(none)>
2019-10-15 11:53:46 +0300

    Fixed all notes bugs

--- GrishaGame.py
+++ GrishaGame.py
@@ -4,7 +4,7 @@ import time
 import math
 
 root=Tk()
-root.geometry('800x600')
+root.geometry('800x620')
 canv=Canvas(root, bg='white')
 canv.pack(fill=BOTH, expand=1)
 
@@ -71,6 +71,7 @@ def enemymove():
 		stoneX=[randint(100, 700), randint(100, 700), randint(100, 700), randint(100, 700)]  #Создание координат препятствий
 		stoneY=[randint(100, 500), randint(100, 500), randint(100, 500), randint(100, 500)]
 		stoneR=[randint(30, 70), randint(30, 70), randint(30, 70), randint(30, 70)]
+		canv.create_rectangle(0, 0, 800, 600, fill='white')
 		for i in range(0, Nstone):
 			Stone(stoneX[i], stoneY[i], stoneR[i])
 	tt=tt+1
@@ -80,45 +81,66 @@ def enemymove():
 		root.after(int(110-10*speed), enemymove)
 		
 def Flybullet():
-	global Xb, Yb, tb, Xtarget, Ytarget, enemyX, enemyY, Score, speed
-	delete_bul(Xb, Yb)
+	global Xb, Yb, tb, Xtarget, Ytarget, enemyX, enemyY, Score, speed, crash
+	if crash==0:
+		delete_bul(Xb, Yb)
 	Xb=800-(800-Xtarget)*tb/33
 	Yb=600-(600-Ytarget)*tb/33
 	for i in range(0, Nstone):
 		if (Xb-stoneX[i])*(Xb-stoneX[i])+(Yb-stoneY[i])*(Yb-stoneY[i])<(stoneR[i]+5)*(stoneR[i]+5):
-			tb=32
+			crash=1
 	for i in range(0, Nenemy):
 		if (Xb-enemyX[i])*(Xb-enemyX[i])+(Yb-enemyY[i])*(Yb-enemyY[i])<225:
-			tb=32
 			delete_enemy(enemyX[i], enemyY[i])
 			enemyX[i]=2000
 			Score=Score+speed
 			ggg='ваш счет:    '+ str(Score)
 			l['text']=ggg
-	bul(Xb, Yb)
+			crash=1
+	if crash==0 and tb<32:
+		bul(Xb, Yb)
+	if tb<32:
+		lll['text']='Идет перезарядка'
+	else:
+		lll['text']='Орудие  готово  к  выстрелу'
 	cannon('black', Xtarget, Ytarget)
 	tb=tb+1
 	if tb<33:
 		root.after(33, Flybullet)
-	if tb==30:
-		delete_bul(Xb, Yb)
 
 def shoot(event):
-	global Xtarget, Ytarget, tb, Xb, Yb
-	cannon('white', Xtarget, Ytarget)
-	delete_bul(Xb, Yb)
-	Xtarget=event.x
-	Ytarget=event.y
-	tb=0
+	global Xtarget, Ytarget, tb, Xb, Yb, crash, StartPlay
+	if tb>32 and StartPlay==1:
+		cannon('white', Xtarget, Ytarget)
+		Xtarget=event.x
+		Ytarget=event.y
+		while Xtarget>0 and Ytarget>0:
+			Xtarget=Xtarget*1.1-80
+			Ytarget=Ytarget*1.1-60
+		tb=0
+		crash=0
+		Xb=800
+		Yb=600
+		cannon('black', Xtarget, Ytarget)
+		Flybullet()
+
+def startgame(event):
+	global stoneX, stoneY, stoneR, Nstone, StartPlay
+	canv.create_rectangle(0, 0, 800, 600, fill='white')
+	for i in range(0, Nstone):
+		Stone(stoneX[i], stoneY[i], stoneR[i])
+	StartPlay=1
 	cannon('black', Xtarget, Ytarget)
-	Flybullet()
+	enemymove()
 
 Score=0
 speed=5
+StartPlay=0
 
 Xb=800  #координаты пули
 Yb=600
-tb=0
+tb=35
+crash=0
 Xtarget=400
 Ytarget=300
 
@@ -133,13 +155,19 @@ stoneX=[randint(100, 700), randint(100, 700), randint(100, 700), randint(100, 70
 stoneY=[randint(100, 500), randint(100, 500), randint(100, 500), randint(100, 500)]
 stoneR=[randint(30, 70), randint(30, 70), randint(30, 70), randint(30, 70)]
 
-for i in range(0, Nstone):
-	Stone(stoneX[i], stoneY[i], stoneR[i])
-
-cannon('black', Xtarget, Ytarget)
+canv.create_text(350, 300, text='Цель  игры  стрелять  по  движущимся  синим  шарам.')
+canv.create_text(350, 320, text='Красные  большие  шары - препятствия.  Выстрел - ')
+canv.create_text(350, 340, text='щелчок  мыши.  Когда  все  шары  пройдут  мимо  вас')
+canv.create_text(350, 360, text='или  умрут  под  вашим  огнем  или  ваша  дуэль  ')
+canv.create_text(350, 380, text='затянется  на  достаточно  большое  время.  Начнется ')
+canv.create_text(350, 400, text='новый  раунд.  Игра - несколько  раундов')
+canv.create_text(350, 480, text='Готовы? Нажмите Tab+пробел!')
 l=Label(root, bg='blue', fg='white', width=100)
 l['text']='ваш счет:   0'
 l.pack()
-enemymove()
+lll=Label(root, bg='blue', fg='white', width=100)
+lll['text']='Орудие  готово  к  выстрелу'
+lll.pack()
+canv.bind('<space>', startgame)
 canv.bind('<Button-1>', shoot)
 mainloop()
